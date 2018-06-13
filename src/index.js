d261ec944b70f578962f00837c712065fed3477d
Vadim Nicolaev <vadim.vicolaev.v@gmail.com>
2018-06-13 21:10:26 +0300

    delete unneccessary depend.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
-import "semantic-ui-css/semantic.min.css";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
