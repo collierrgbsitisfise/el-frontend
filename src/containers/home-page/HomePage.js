e375d7ed03aa3193453aa7a0aa337de6965cb6ab
Vadim Nicolaev <vadim.vicolaev.v@gmail.com>
2018-06-15 23:35:51 +0300

    implement logic to validate link on clinet side

--- src/containers/home-page/HomePage.js
+++ src/containers/home-page/HomePage.js
@@ -5,6 +5,8 @@ import { connect } from "react-redux";
 import { getLink } from "../../actions/link";
 import "./home-page.css";
 
+const LINK_REG_EXP = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+
 class HomePage extends React.Component {
   
   componentDidMount(){
@@ -13,9 +15,15 @@ class HomePage extends React.Component {
   
   constructor(props) {
     super(props)
-    
+    this.state = {
+      link: '',
+    }
   }
   
+  handleChange = e => this.setState({
+    link: e.target.value
+  });
+
   render () {
     
     const {
@@ -23,17 +31,36 @@ class HomePage extends React.Component {
     } = this.props;
 
     return (
-      <div className="home-page" onClick={() => {
-          getLink('http://google.com');
-        }}>
+      <div className="home-page">
         <input
+          value={this.state.link}
+          onChange={this.handleChange}
           ref={(input) => {this.nameInput = input; }}
           type="text"
           className="link-input"
           placeholder="Enter Link Address"/>
-          <button className="btn action">
-              get short link
-          </button>
+          {
+            LINK_REG_EXP.test(this.state.link) ? 
+              (
+                <button
+                  onClick={() => {
+                    getLink(this.state.link);
+                  }}
+                  className={"btn action " + (!this.state.link ? 'not-visible' : '')}>
+                    get short link
+                </button>
+              ) :
+              (
+                <button
+                  onClick={() => {
+                    console.log('it is state');
+                    console.log(this.state.link);
+                  }}
+                  className={"btn incorrect " + (!this.state.link ? 'not-visible' : '')}>
+                    Incorrect Link
+                </button>
+              )
+          }
       </div>
     ); 
   }
