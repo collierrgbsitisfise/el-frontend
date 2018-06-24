fa503cb6217ae4aea27a8af65096be4f24ce9627
Vadim Nicolaev <vadim.vicolaev.v@gmail.com>
2018-06-24 21:49:46 +0300

    change query param name for create link request

--- src/api/link.api.js
+++ src/api/link.api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import CONGIF from "../config";
 
 const getShortLink = (link,privateOnly,isOnceAvailable) =>
-    axios.get(`${CONGIF.API_ENDPOINT}/api/v1/es-link/create-es-link?link=${link}&privateOnly=${privateOnly}&isOnceAvailable=${isOnceAvailable}`);
+    axios.get(`${CONGIF.API_ENDPOINT}/api/v1/es-link/create-es-link?link=${link}&privateOnly=${privateOnly}&onceAvailable=${isOnceAvailable}`);
 
 
 export default {
